c6d5f577d81a26b3c15151d6e1f8a04fc4958b64
Markus Felten <Markus.Felten@gmx.de>
2016-01-09 23:57:20 +0100

    test: TimeoutInterceptor

--- tests/interceptor_test.js
+++ tests/interceptor_test.js
@@ -23,11 +23,14 @@ function dummyEndpoint(name) {
   return {get name() {
       return name;
     },
+    toString() {
+      return this.name;
+    },
     "step": logger
   };
 }
 
-describe('interceptors', function () {
+describe('interceptors', () => {
   const ep = dummyEndpoint('ep');
 
   mochaInterceptorTest(Interceptor, ep, {}, "none");
@@ -38,42 +41,40 @@ describe('interceptors', function () {
   mochaInterceptorTest(LimitingInterceptor, ep, {
     limit: 5
   }, "request-limit", itc => {
-    it('has limit', function () {
-      assert.equal(itc.limit, 5);
-    });
+    it('has limit', () => assert.equal(itc.limit, 5));
   });
 
   const itc = mochaInterceptorTest(TimeoutInterceptor, ep, {
-    timeout: 12345
+    timeout: 15
   }, "timeout", itc => {
-    it('has timeout', function () {
-      assert.equal(itc.timeout, 12345);
-    });
+    it('has timeout', () => assert.equal(itc.timeout, 15));
 
     itc.connected = dummyEndpoint('ep');
 
+    // request value is the timeout
     itc.connected.receive = request =>
       new Promise((fullfilled, rejected) =>
-        setTimeout(() => fullfilled(request), 10));
+        setTimeout(() => fullfilled(request), request));
 
-    xit('long running timout request', function (done) {
-      let response = itc.receive("request", 5);
+    it('passing request within time', done => {
+      const response = itc.receive(5); // wait 5 msecs then fullfill
+      response.then(resolved => {
+        done();
+      }).catch(rejected => {
+        //console.log(`got timeout ? ${rejected}`);
+        done(rejected);
+      });
+    });
+
+    it('rejecting long running request', done => {
+      const response = itc.receive(100); // wait 100 msecs then fullfill -> timeout
       response.then(resolved => {
         console.log(`resolved ${resolved}`);
       }).catch(rejected => {
-        console.log(`got timeout ? ${rejected}`);
+        //console.log(`got timeout ? ${rejected}`);
         done();
       });
     });
 
-    xit('passing timout request', function (done) {
-      let response = itc.receive("request", 100);
-      response.then(resolved => {
-        console.log(`resolved ${resolved}`);
-      }).catch(rejected => {
-        console.log(`got timeout ? ${rejected}`);
-        done();
-      });
-    });
   });
 });
