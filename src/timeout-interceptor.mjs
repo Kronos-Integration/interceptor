e7d2615b9463a1765abac9a852094909007a2550
Markus Felten <Markus.Felten@gmx.de>
2020-04-23 14:54:18 +0200

    docs: typo

--- src/timeout-interceptor.mjs
+++ src/timeout-interceptor.mjs
@@ -31,7 +31,7 @@ export class TimeoutInterceptor extends Interceptor {
 }
 
 /**
- * Rejects promise when it ia not resolved within given timeout
+ * Rejects promise when it is not resolved within given timeout
  * @param {Promise} promise
  * @param {number} timeout in miliseconds
  * @param {string} name
