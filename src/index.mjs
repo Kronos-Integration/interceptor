76fb8402e44eed1db026dca13395ca95545a4e24
Markus Felten <Markus.Felten@gmx.de>
2020-12-03 21:04:31 +0100

    feat: template interceptor

--- src/index.mjs
+++ src/index.mjs
@@ -3,3 +3,4 @@ export * from "./timeout-interceptor.mjs";
 export * from "./stats-collector-interceptor.mjs";
 export * from "./limiting-interceptor.mjs";
 export * from "./logging-interceptor.mjs";
+export * from "./template-interceptor.mjs";
