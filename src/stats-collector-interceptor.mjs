752ee008a95bc1bdfb9da08f512bcbf8b6946bfa
Markus Felten <Markus.Felten@gmx.de>
2025-07-31 15:10:29 +0100

    fix: remove problematic private fields

--- src/stats-collector-interceptor.mjs
+++ src/stats-collector-interceptor.mjs
@@ -12,45 +12,19 @@ export class StatsCollectorInterceptor extends Interceptor {
     return 'collect-request-stats';
   }
 
-  #numberOfRequests;
-  #numberOfFailedRequests;;
-  #minRequestProcessingTime;;
-  #maxRequestProcessingTime;
-  #totalRequestProcessingTime;
-
   reset() {
-    this.#numberOfRequests = 0;
-    this.#numberOfFailedRequests = 0;
-    this.#minRequestProcessingTime = Number.MAX_VALUE;
-    this.#maxRequestProcessingTime = 0;
-    this.#totalRequestProcessingTime = 0;
-  }
-
-  get numberOfRequests() {
-    return this.#numberOfRequests;
-  }
-
-  get numberOfFailedRequests() {
-    return this.#numberOfFailedRequests;
-  }
-
-  get maxRequestProcessingTime() {
-    return this.#maxRequestProcessingTime;
-  }
-
-  get minRequestProcessingTime() {
-    return this.#minRequestProcessingTime;
-  }
-
-  get totalRequestProcessingTime() {
-    return this.#totalRequestProcessingTime;
+    this.numberOfRequests = 0;
+    this.numberOfFailedRequests = 0;
+    this.minRequestProcessingTime = Number.MAX_VALUE;
+    this.maxRequestProcessingTime = 0;
+    this.totalRequestProcessingTime = 0;
   }
 
   /**
    * Logs the time the requests takes
    */
   async receive(endpoint,...args) {
-    this.#numberOfRequests += 1;
+    this.numberOfRequests += 1;
 
     const start = Date.now();
 
@@ -58,20 +32,20 @@ export class StatsCollectorInterceptor extends Interceptor {
       const response = await this.connected.receive(...args);
       const now = Date.now();
       const pt = now - start;
-      this.#totalRequestProcessingTime += pt;
+      this.totalRequestProcessingTime += pt;
 
-      if (pt > this.#maxRequestProcessingTime) {
-        this.#maxRequestProcessingTime = pt;
+      if (pt > this.maxRequestProcessingTime) {
+        this.maxRequestProcessingTime = pt;
       }
 
-      if (pt < this.#minRequestProcessingTime) {
-        this.#minRequestProcessingTime = pt;
+      if (pt < this.minRequestProcessingTime) {
+        this.minRequestProcessingTime = pt;
       }
 
       endpoint.logger.debug(level=>`took ${pt} ms for ${[...args]}`);
       return response;
     } catch (err) {
-      this.#numberOfFailedRequests += 1;
+      this.numberOfFailedRequests += 1;
       throw err;
     }
   }
